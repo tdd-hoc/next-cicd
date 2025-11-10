6a45c1a9e26a35851b0921d24556524f702b7d42
DateDuy <thachdateduy@gmail.com>
2025-11-11 00:00:18 +0700

    update page

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -4,13 +4,26 @@ type Params = {
   };
 };
 
+// 🧩 Thêm đoạn này để Next.js biết build các slug nào
+export async function generateStaticParams() {
+  // ⚙️ Tạm thời fix cứng vài giá trị slug để export tĩnh
+  return [
+    { slug: "hello-world" },
+    { slug: "nextjs-16" },
+    { slug: "thachdateduy" },
+  ];
+}
+
 export async function generateMetadata({ params }: Params) {
   return { title: `Post: ${params.slug}` };
 }
 
 export default function Page({ params }: Params) {
-  return <>
-  <h1>Slug: {params.slug}</h1><p>thachdateduy</p>
-  <p>socruong</p>
-  </>;
+  return (
+    <>
+      <h1>Slug: {params.slug}</h1>
+      <p>thachdateduy</p>
+      <p>socruong</p>
+    </>
+  );
 }
